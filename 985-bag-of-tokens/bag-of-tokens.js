7d59642ba4c64854619d196f809a0c5fdd84d0c6
Maksim Kazak <kazakmaksims@gmail.com>
2024-03-04 19:18:33 +0100

    Time: 68 ms (28.57%) | Memory: 53.3 MB (14.29%) - LeetSync

---
+++ 985-bag-of-tokens/bag-of-tokens.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} tokens
+ * @param {number} power
+ * @return {number}
+ */
+var bagOfTokensScore = function(tokens, power) {
+    const sorted = tokens.sort((a, b) => a - b);
+    console.log(sorted, power);
+    let maxScore = 0, currentScore = 0;
+    while (power >= tokens[0] || currentScore > 0) {
+        if (power >= tokens[0]) {
+            power -= tokens.shift();
+            currentScore++;
+        } else if (currentScore) {
+            power += tokens.pop();
+            currentScore--;
+        }
+        maxScore = currentScore > maxScore ? currentScore : maxScore;
+    }
+    return maxScore;
+};

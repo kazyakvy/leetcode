f95ee1b9521966872f699cb37c03301e7082f56d
Maksim Kazak <kazakmaksims@gmail.com>
2024-03-10 21:37:51 +0100

    Time: 61 ms (27.82%) | Memory: 51.3 MB (12.21%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -10,16 +10,11 @@ var intersection = function(nums1, nums2) {
             map1[value] = true;
         }
     }
-    const map2 = {};
-    for (let value of nums2) {
-        if (!map2[value]) {
-            map2[value] = true;
-        }
-    }
     const result = [];
-    for (let key in map1) {
-        if (map1[key] && map2[key]) {
-            result.push(key);
+    const set = new Set(nums2);
+    for (let value of set) {
+        if (map1[value]) {
+            result.push(value);
         }
     }
     return result;

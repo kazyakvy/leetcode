8e29865e5cef768272b9da60ce35aa49952a10ba
Maksim Kazak <kazakmaksims@gmail.com>
2024-03-18 21:02:36 +0100

    Time: 84 ms (59.76%) | Memory: 57.6 MB (5.64%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -6,18 +6,18 @@ var sortedSquares = function(nums) {
     const stack = [], result = [];
     
     for (let i = 0; i < nums.length; i++) {
-        const currentSquared = nums[i]**2;
+        const current = nums[i]**2;
         if (nums[i] < 0) {
-            stack.push(currentSquared);
+            stack.push(current);
             continue;
         }
         if (stack.length) {
-            while (stack[stack.length - 1] < currentSquared) {
+            while (stack[stack.length - 1] < current) {
                 const value = stack.pop();
                 result.push(value);
             }
         }
-        result.push(currentSquared);
+        result.push(current);
     }
     
     if (stack.length) {

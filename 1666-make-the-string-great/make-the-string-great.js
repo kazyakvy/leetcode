c6bfa1483c6c5493280bf68bdb20f1586a9b7641
Maksim Kazak <kazakmaksims@gmail.com>
2024-04-05 23:35:35 +0200

    Time: 64 ms (32.01%) | Memory: 50.9 MB (62.69%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.js
+++ 1666-make-the-string-great/make-the-string-great.js
@@ -2,32 +2,16 @@
  * @param {string} s
  * @return {string}
  */
-const lettersValid = (a, b) => {
-    if (a === b) {
-        return true;
-    }
-    if (a.toLowerCase() === b.toLowerCase()) {
-        return false;
-    }
-    return true;
-};
-
-const isGreat = (s) => {
-    for (let i = 0; i <= s.length - 2; i++) {
-        if (!lettersValid(s[i], s[i + 1])) {
-            return false;
-        }
-    }
-    return true;
-};
-
 var makeGood = function(s) {
-    while (!isGreat(s)) {
-        for (let i = 0; i < s.length - 1; i++) {
-            if (!lettersValid(s[i], s[i + 1])) {
-                s = s.split(s[i] + s[i + 1]).join('');
-            }
+    const stack = [];
+    
+    for (const char of s) {
+        if (stack.length > 0 && Math.abs(char.charCodeAt() - stack[stack.length - 1].charCodeAt()) === 32) {
+            stack.pop();
+        } else {
+            stack.push(char);
         }
     }
-    return s;
+    
+    return stack.join('');
 };

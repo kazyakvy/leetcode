81f3b1654a12d7b3941ae2695a1d6d31242b28a5
Maksim Kazak <kazakmaksims@gmail.com>
2024-04-09 16:24:06 +0200

    Time: 54 ms (63.39%) | Memory: 49 MB (60.66%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.js
@@ -5,12 +5,12 @@
  */
 var timeRequiredToBuy = function(tickets, k) {
     const val = tickets[k];
-    return tickets.reduce((acc, el, i, arr) => {
+    return tickets.reduce((acc, el, i) => {
         if (i < k) {
-            return acc + (arr[i] < val ? arr[i] : val);
+            return acc + (el < val ? el : val);
         }
         if (i > k) {
-            return acc + (arr[i] < val ? arr[i] : val - 1);
+            return acc + (el < val ? el : val - 1);
         }
         return acc + val;
     }, 0);

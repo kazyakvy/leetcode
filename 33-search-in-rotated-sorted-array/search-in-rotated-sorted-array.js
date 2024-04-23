8bb8dbde8d3d3a7ae788f5ae5d38e7b8d3c59161
Maksim Kazak <kazakmaksims@gmail.com>
2024-04-23 23:24:50 +0200

    Time: 54 ms (49.19%) | Memory: 48.3 MB (93.69%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -4,10 +4,29 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
+    let low = 0, high = nums.length - 1;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
     }
+
     return -1;
 };

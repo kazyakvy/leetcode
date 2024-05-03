71d6b068b6624b3266bd7976f40176d515edad8a
Maksim Kazak <kazakmaksims@gmail.com>
2024-05-03 12:02:17 +0200

    Time: 53 ms (51.92%) | Memory: 49.1 MB (18.46%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.js
+++ 165-compare-version-numbers/compare-version-numbers.js
@@ -7,9 +7,11 @@ var compareVersion = function(version1, version2) {
     const v1 = version1.split('.');
     const v2 = version2.split('.');
     const n = v1.length > v2.length ? v1.length : v2.length;
+
     for (let i = 0; i < n; i++) {
-        const num1 = +(v1[i] || 0);
-        const num2 = +(v2[i] || 0);
+        const num1 = +v1[i] || 0;
+        const num2 = +v2[i] || 0;
+        
         if (num1 === num2) {
             continue;
         }
@@ -18,5 +20,6 @@ var compareVersion = function(version1, version2) {
         }
         return 1;
     }
+
     return 0;
 };

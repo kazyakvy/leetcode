0b956f2312698033a06fb9377a7f3e6ec9b36b08
Maksim Kazak <kazakmaksims@gmail.com>
2024-05-09 19:18:17 +0200

    Time: 249 ms (38.36%) | Memory: 66.8 MB (93.61%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function(happiness, k) {
+    happiness.sort((a, b) => b - a);
+    let result = 0;
+
+    for (let i = 0; i < k; i++) {
+        const val = happiness[i] - i;
+        result += val >= 0 ? val : 0;
+    }
+
+    return result;
+};
